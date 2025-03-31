d86a58b2a899e68166ced7dec6ea68347a8401ba
Leo Nickl <leo@leonickl.de>
2025-03-31 20:14:26 +0200

    re-add soft deletes

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
 
 /**
@@ -12,4 +13,7 @@ use Illuminate\Support\Collection;
  * @method static Builder<static> whereNull($columns, $boolean = 'and', $not = false)
  * @method static Collection<int, static> all(string[] $columns = '*')
  */
-abstract class Model extends \Illuminate\Database\Eloquent\Model {}
+abstract class Model extends \Illuminate\Database\Eloquent\Model
+{
+    use SoftDeletes;
+}
