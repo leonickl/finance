6d5312ea373501998c5852abee7dfe00a53e8b5d
Leo Nickl <leo@leonickl.de>
2025-04-16 16:43:13 +0200

    add red flag indicating development mode

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,13 @@
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
+        @if(env('APP_TYPE') === "dev")
+            <!-- Red Flag in Page Corner -->
+            <div class="fixed top-0 left-0 bg-red-600 text-white text-sm md:text-base font-bold px-10 py-3 rounded-br-lg z-50 shadow-lg">
+                BETA
+            </div>
+        @endif
+
         @inertia
     </body>
 </html>
